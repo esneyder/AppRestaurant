2a1d28204e889fbf0a3bb1423e5696b59a7c4c96
Edwin Esneyer Alvarez <esneyder@users.noreply.github.com>
2016-05-10 18:10:01 -0500

    Update Config.java

--- app/src/main/java/com/developer/webapiclient/utl/Config.java
+++ app/src/main/java/com/developer/webapiclient/utl/Config.java
@@ -4,6 +4,6 @@ package com.developer.webapiclient.utl;
  * Created by Developer on 09/05/2016.
  */
 public class Config {
-public static  String dataUrl = "https://microsoft-apiapp454fa0f32a954e0a8fc8b56746f1a46a.azurewebsites.net/institute/";
+public static  String dataUrl = "URL o ip del servicio/institute/";
 
 }
